fa3230b62037b9b1b355c861e0206ba327313a97
ysuovnii <ysuovnii@gmail.com>
2026-08-13 19:43:48 +0530

    remove cmd/keyboard merge in single main file

--- src/keyboard/keyboard.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-namespace Keyboard {
-    bool writeFile(const std::string& path, const std::string& value);
-    bool setBrightness(int level);
-    bool setRGB(int mode, int red, int green, int blue, int speed);
-}
